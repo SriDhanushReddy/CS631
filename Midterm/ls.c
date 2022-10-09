d59a8d8cc4f2f7def8cf183f299ffcbd99436247
Sri Dhanush Reddy <37323159+SriDhanushReddy@users.noreply.github.com>
2022-10-08 22:45:02 -0400

    Update ls.c

--- Midterm/ls.c
+++ Midterm/ls.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include "ls.h"
 
-int main(){
+#define NOTPRINT 1
+
+int A_flag, a_flag, c_flag, d_flag, F_flag, f_flag, h_flag, i_flag;
+int k_flag, l_flag, n_flag, q_flag, R_flag, r_flag, S_flag, s_flag;
+int t_flag, u_flag, w_flag, one_flag, C_flag, x_flag; 
+int to_terminal_flag; 
+int dirprint_total;
+int regular_file_total;
+int totalprint_flag;
+int readoption;
+int dirprint;
+int termwidth = 80;
+float blktimes;
+
+int main
+(){
 	
 	return 0;
 }
