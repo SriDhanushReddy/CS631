175b9cf1d43bfbeefbc913eadf51ff6c4e9d2398
Sri Dhanush Reddy <37323159+SriDhanushReddy@users.noreply.github.com>
2022-10-08 22:45:52 -0400

    Update ls.c

--- Midterm/ls.c
+++ Midterm/ls.c
@@ -14,8 +14,31 @@ int dirprint;
 int termwidth = 80;
 float blktimes;
 
+static char *pro_name;
+static void (*display)(void);
+static int (*compare)(const FTSENT *a, const FTSENT *b);
+
 int main
-(){
-	
+(int argc, char *argv[]){
+	int    		   c;
+	char 		   dot[] = ".", *dotav[] = { dot, (char *)NULL };
+	struct winsize win;
+	FTS 		   *ftsp;
+    FTSENT         *p, *tmp;
+
+	pro_name = *argv;
+	r_flag     = 1;
+	C_flag     = 1;
+	display    = orderdisplay;
+	compare    = cmpname;
+	readoption = FTS_PHYSICAL;
+	blktimes   = 1;
+
+	/* 
+	** determine output to terminal or file
+	** meantime if output is not a terminal 
+	** set w, 1 flag to 1
+	*/
+
 	return 0;
 }
