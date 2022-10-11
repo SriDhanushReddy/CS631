243b54a05857c77ee738714f375cd51b9f2be563
Sri Dhanush Reddy <37323159+SriDhanushReddy@users.noreply.github.com>
2022-10-10 22:17:35 -0400

    Update ls.h

--- Midterm/ls.h
+++ Midterm/ls.h
@@ -1,1 +1,25 @@
 
+#ifndef ls_h
+#define ls_h
+
+int list_all();
+int all_unhidden();
+int compare(const struct dirent **a, const struct dirent **b);
+void print_sorted(char *path);
+void print_dir_contents(char *dir);
+void show_dir( const char *dir_name );
+void list_files_unsorted(const char *path);
+void printFileInode(char* file);
+int printFileInode2();
+int list_directory_contents(const char *dirname);
+int list_directory_contents2(const char *dirname);
+int non_printable_all();
+void depthFirst(DIR *dir);
+int isdirectory(char *path);
+void print_directory_contents3(char *dir);
+int sorting_function();
+void file_size();
+int file_sort();
+int sortByLastAccess(const char *dirname);
+
+#endif
